065764b16c9c27dc617ee6c4206928d189d127d8
FoXuPPer <149265826+FoXuPPer@users.noreply.github.com>
2025-04-02 20:56:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # Функция для команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("Запустить Speedtest", web_app={"url": "https://<your-mini-app-url>"})]
+        [InlineKeyboardButton("Запустить Speedtest", web_app={"url": "http://speedtest.tele2.net/10MB.zip"})]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
